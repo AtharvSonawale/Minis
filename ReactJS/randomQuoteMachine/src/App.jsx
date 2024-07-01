5b2750966456af7222229c1a5bf75b04821e0a84
AtharvSonawale <atharvsonawalexyz@gmail.com>
2024-07-01 22:34:44 +0530

    second commit

--- ReactJS/randomQuoteMachine/src/App.jsx
+++ ReactJS/randomQuoteMachine/src/App.jsx
@@ -1,33 +1,38 @@
-
 import './App.css'
+import Twitter from '../png/sl_z_072523_61700_05-removebg-preview.png'
+import Linkedin from '../png/linkedin-logo-png-2026.png'
 
 function App() {
   return (
-    <div className="wrapper">
-      <img src="" alt="" />
-      <div id="quote-box">
-        <div id="quote-text">
-          <p className=""><span id="text" className='text-3xl-bold'>Don't settle for what life gives you, make life better and build something.</span></p>
-        </div>
+    <div>
+      <div className='w-screen h-screen border-4 border-zinc-700 text-center flex flex-col'>
+        <div id="quote-box" className=' w-40-screen h-auto mx-auto
+        my-auto mb-0 border-2 border-zinc-700 rounded-lg bg-yellow-500'>
+          <div id="quote-text">
+            <p className="m-4 font-medium text-3xl text-white"><span id="text">Don't settle for what life gives   you, make life better and build something.
+              </span></p>
+          </div>
 
-        <div className="flex justify-end">- Ashton Kutcher<span id="author"></span></div>
+          <div className="flex justify-end mr-4 text-xl text-white">- Ashton Kutcher<span id="author"></  span></div>
 
-        <div className="flex justify-between">
+          <div className="flex justify-between place">
 
-          <div className="left">
-          <a href="" className="fa fa-twitter" id="tweet-quote" title='Tweet this quote!' target='_top'><i className="">&#xf099;
-          </i></a>
+            <div className="mx-4 my-4 flex p-1 items-center jus">
+              <a href="" className="p-2 bg-white rounded-lg" id="tweet-quote" title='Tweet this quote!'   target='_top'>
+              <img src={Twitter} alt="" className='w-4 h-4 transition duration-750 hover:opacity-75'/></a>
 
-          <a href="" className="button" id='linkedin-quote' title='Linkedin this quote!' target='_blank'><i className="">&#xf099;</i></a>
+              <a href="" className="" id='linkedin-quote' title='Linkedin this quote!' target='_blank'>
+              <img src={Linkedin} alt="" className='w-8 h-8 transition duration-750 hover:opacity-75 ml-2   rounded-lg'/></a>
           
-          </div>
+              </div>
 
-          <div className="right">
-          <button className="button" id="new-quote">New quote</button>
+              <div className="right">
+              <button className="m-4 border border-gray-300 rounded-lg p-2 text-gray-800 bg-white font-bold" id="new-quote">New quote</button>
+              </div>
+            </div>
           </div>
+          <footer className='m-4 text-white mb-40'>by <a href="" target='_blank' className='font-bold'>Atharv Sonawale</a></footer>
         </div>
-      </div>
-        <footer className='footer'>by <a href="" target='_blank'>Atharv Sonawale</a></footer>
     </div>
   );
 }
