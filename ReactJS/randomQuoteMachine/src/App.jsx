aa1cfcc8a9d0f3f74680ae2645489f3f3c8499eb
AtharvSonawale <atharvsonawalexyz@gmail.com>
2024-07-04 21:54:40 +0530

    added functionality

--- ReactJS/randomQuoteMachine/src/App.jsx
+++ ReactJS/randomQuoteMachine/src/App.jsx
@@ -1,40 +1,120 @@
-import './App.css'
-import Twitter from '../png/sl_z_072523_61700_05-removebg-preview.png'
-import Linkedin from '../png/linkedin-logo-png-2026.png'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Twitter from '../png/sl_z_072523_61700_05-removebg-preview.png';
+import Linkedin from '../png/linkedin-logo-png-2026.png';
 
-function App() {
+const colors = [
+  '#9e0142',
+  '#d53e4f',
+  '#f46d43',
+  '#fdae61',
+  '#fee08b',
+  '#e6f598',
+  '#abdda4',
+  '#66c2a5',
+  '#3288bd',
+  '#5e4fa2'
+];
+
+const QuoteBox = () => {
+  const [quotesData, setQuotesData] = useState([]);
+  const [currentQuote, setCurrentQuote] = useState('');
+  const [currentAuthor, setCurrentAuthor] = useState('');
+  const [color, setColor] = useState(colors[0]);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
+  const getQuotes = async () => {
+    try {
+      const response = await axios.get(
+        'https://api.jsonsilo.com/demo/48148710-f7b9-46dd-b09d-b3f0e4c0bb15'
+      );
+      setQuotesData(response.data.quotes);
+      getQuote(response.data.quotes);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getRandomQuote = () => {
+    return quotesData[Math.floor(Math.random() * quotesData.length)];
+  };
+
+  const getQuote = (quotes = quotesData) => {
+    const randomQuote = getRandomQuote();
+    setCurrentQuote(randomQuote.quote);
+    setCurrentAuthor(randomQuote.author);
+    const newColor = colors[Math.floor(Math.random() * colors.length)];
+    setColor(newColor);
+  };
+
+  const handleNewQuote = () => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      setIsAnimating(false);
+      getQuote();
+    }, 1000);
+  };
+
   return (
-    <div>
-      <div className='w-screen h-screen border-4 border-zinc-700 text-center flex flex-col'>
-        <div id="quote-box" className=' w-40-screen h-auto mx-auto
-        my-auto mb-0 border-2 border-zinc-700 rounded-lg bg-yellow-500'>
-          <div id="quote-text">
-            <p className="m-4 font-medium text-3xl text-white"><span id="text">Don't settle for what life gives   you, make life better and build something.
-              </span></p>
-          </div>
-
-          <div className="flex justify-end mr-4 text-xl text-white">- Ashton Kutcher<span id="author"></  span></div>
-
-          <div className="flex justify-between place">
-
-            <div className="mx-4 my-4 flex p-1 items-center jus">
-              <a href="" className="p-2 bg-white rounded-lg" id="tweet-quote" title='Tweet this quote!'   target='_top'>
-              <img src={Twitter} alt="" className='w-4 h-4 transition duration-750 hover:opacity-75'/></a>
-
-              <a href="" className="" id='linkedin-quote' title='Linkedin this quote!' target='_blank'>
-              <img src={Linkedin} alt="" className='w-8 h-8 transition duration-750 hover:opacity-75 ml-2   rounded-lg'/></a>
-          
-              </div>
-
-              <div className="right">
-              <button className="m-4 border border-gray-300 rounded-lg p-2 text-gray-800 bg-white font-bold" id="new-quote">New quote</button>
-              </div>
-            </div>
-          </div>
-          <footer className='m-4 text-white mb-40'>by <a href="" target='_blank' className='font-bold'>Atharv Sonawale</a></footer>
+    <div
+      className={`flex items-center justify-center min-h-screen flex-col ${isAnimating ? 'bg-transition' : ''}`}
+      style={{ backgroundColor: color, color: color }}
+    >
+      <div
+        id="quote-box"
+        className="bg-white p-8 rounded shadow-md w-96"
+        style={{ color: color }}
+      >
+        <div className="quote-text text-center text-2xl mb-4">
+          <i className="fa fa-quote-left mr-2"></i>
+          <span id="text">{currentQuote}</span>
         </div>
+        <div className="quote-author text-right mb-4">- <span id="author">{currentAuthor}</span></div>
+        <div className="flex justify-between">
+          <div className="w-auto h-auto flex p-0.5">
+            <a
+              className="button bg-gray-600 text-white py-2 px-2 rounded hover:opacity-75 mr-4"
+              id="tweet-quote"
+              title="Tweet this quote!"
+              target="_top"
+              href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="${encodeURIComponent(
+                currentQuote
+              )}" ${encodeURIComponent(currentAuthor)}`}
+            >
+              <i className="fa fa-twitter"><img src={Twitter} alt="" className="w-5 h-5"/></i>
+            </a>
+            <a
+              className="button text-white rounded hover:opacity-75"
+              id="tumblr-quote"
+              title="Post this quote on tumblr!"
+              target="_blank"
+              href={`https://www.linkedin.com/in/overlay/?posttype=quote&tags=quotes,freecodecamp&caption=${encodeURIComponent(
+                currentAuthor
+              )}&content=${encodeURIComponent(
+                currentQuote
+              )}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`}
+            >
+              <i><img src={Linkedin} alt="" className="w-9 h-9"/></i>
+            </a>
+          </div>
+          <button
+            className="button bg-gray-700 text-opacity-0 font-bold py-2 px-4 rounded hover:bg-opacity-75"
+            id="new-quote"
+            onClick={handleNewQuote}
+          >
+            New quote
+          </button>
+        </div>
+      </div>
+      <div className="footer text-white mt-8">
+        by <a href="https://codepen.io/Atharv-Sonawale" className="font-bold">Atharv Sonawale</a>
+      </div>
     </div>
   );
-}
+};
 
-export default App
+export default QuoteBox;
