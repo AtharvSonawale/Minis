bd70feedd2166ba66716102521d6b53995a4b499
AtharvSonawale <atharvsonawalexyz@gmail.com>
2024-07-07 23:06:39 +0530

    functionality

---
+++ ReactJS/drumMachine/drumPadMachine/components/DrumPad.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentSound } from '../src/store';
+
+const DrumPad = ({ pad }) => {
+  const dispatch = useDispatch();
+  const power = useSelector((state) => state.drum.power);
+  const volume = useSelector((state) => state.drum.volume);
+
+  const playSound = () => {
+    if (power) {
+      const audio = document.getElementById(pad.key);
+      audio.volume = volume;
+      audio.currentTime = 0;
+      audio.play();
+      dispatch(setCurrentSound(pad.id));
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key.toUpperCase() === pad.key) {
+      playSound();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
+  return (
+    <div className="drum-pad" id={pad.id} onClick={playSound}>
+      {pad.key}
+      <audio className="clip" id={pad.key} src={pad.url}></audio>
+    </div>
+  );
+};
+
+export default DrumPad;
