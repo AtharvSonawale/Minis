db43d5590331f466a096bdb2a5c774b4c4ff9448
AtharvSonawale <atharvsonawalexyz@gmail.com>
2024-07-21 17:46:53 +0530

    added some corrections

--- ReactJS/25-5-clock/src/App.jsx
+++ ReactJS/25-5-clock/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import BreakLength from './components/BreakLength';
 import SessionLength from './components/SessionLength';
 import Timer from './components/Timer';
